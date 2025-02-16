8b9b5a6819bd3934ef4143ff948d779b5531d714
EForamiglio <erik.foramiglio.vieira@gmail.com>
2025-02-16 17:28:08 -0300

    Adding unit tests for validator

--- src/test/java/com/pedrik/validator/service/ValidatorServiceTest.java
+++ src/test/java/com/pedrik/validator/service/ValidatorServiceTest.java
@@ -1,7 +1,43 @@
 package com.pedrik.validator.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ValidatorServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorServiceTest {
+
+    private final ValidatorService validatorService = new ValidatorService();
+
+    @ParameterizedTest
+    @MethodSource("validDocuments")
+    @DisplayName("Deve retornar true para documentos válidos")
+    void shouldReturnTrueForValidDocument(String document) {
+        assertTrue(validatorService.run(document));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidDocuments")
+    @DisplayName("Deve retornar false para documentos inválidos")
+    void shouldReturnFalseForInvalidDocument(String document) {
+        assertFalse(validatorService.run(document));
+    }
+
+    // Valid documents source
+    static Stream<String> validDocuments() {
+        return Stream.of("25.625.843-0", "37.787.067-5", "293285639", "183622534");
+    }
+
+    // Invalid documents source
+    static Stream<String> invalidDocuments() {
+        return Stream.of("25.625.AAA-0", "37.700.000-X", "293285222", "18362");
+    }
 }
+
