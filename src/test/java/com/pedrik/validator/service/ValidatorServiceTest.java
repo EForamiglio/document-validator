e69df1a9e86a9f1bb6bd2d26062990330eb903e4
EForamiglio <erik.foramiglio.vieira@gmail.com>
2025-02-16 17:37:37 -0300

    Adjusting test params for cpf

--- src/test/java/com/pedrik/validator/service/ValidatorServiceTest.java
+++ src/test/java/com/pedrik/validator/service/ValidatorServiceTest.java
@@ -1,16 +1,13 @@
 package com.pedrik.validator.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidatorServiceTest {
 
@@ -32,12 +29,14 @@ class ValidatorServiceTest {
 
     // Valid documents source
     static Stream<String> validDocuments() {
-        return Stream.of("25.625.843-0", "37.787.067-5", "293285639", "183622534");
+        return Stream.of("25.625.843-0", "37.787.067-5", "293285639", "183622534", /*RG*/
+              /*CPF*/  "207.621.060-18", "16799662009", "46248418080");
     }
 
     // Invalid documents source
     static Stream<String> invalidDocuments() {
-        return Stream.of("25.625.AAA-0", "37.700.000-X", "293285222", "18362");
+        return Stream.of("25.625.AAA-0", "37.700.000-X", "293285222", "18362", /*RG*/
+               /*CPF*/ "43018578033", "046.961.388-11");
     }
 }
 
