d54c7065f283bd554d22fa8fc45ce7d632112ecb
EForamiglio <erik.foramiglio.vieira@gmail.com>
2025-02-16 16:13:40 -0300

    RG validation logic

---
+++ src/main/java/com/pedrik/validator/exception/InvalidDocumentExeception.java
@@ -0,0 +1,8 @@
+package com.pedrik.validator.exception;
+
+public class InvalidDocumentExeception extends Exception{
+
+    public InvalidDocumentExeception(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/pedrik/validator/service/ValidatorService.java
+++ src/main/java/com/pedrik/validator/service/ValidatorService.java
@@ -1,18 +1,20 @@
 package com.pedrik.validator.service;
 
+import com.pedrik.validator.exception.InvalidDocumentExeception;
 import org.springframework.stereotype.Service;
-
-import java.util.LinkedList;
 
 @Service
 public class ValidatorService {
 
-    private int count = 0;
-    private boolean stateFinal = false;
+    private int sumRg;
+    private int poundRg;
 
     public boolean run(final String document) {
         try {
-            validate(document.replace(".", ""));
+            sumRg = 0;
+            poundRg = 2;
+
+            isValidDocument(document);
 
             return true;
         } catch (Exception ex) {
@@ -20,17 +22,49 @@ public class ValidatorService {
         }
     }
 
-    public void validate(final String document) {
-        document.toCharArray();
+    private void isValidDocument(String document) throws Exception {
+        String cleanDoc = document.replace(".", "").replace("-", "");
 
-        checkNumsLogic();
+        if(document.length() == 9) {
+            isValidRG(cleanDoc, 0);
+        } else if (document.length() == 11) {
+            isValidCpf();
+        } else {
+            throw new InvalidDocumentExeception("Documento com muitos caracteres");
+        }
     }
 
-    private void checkNumsLogic() {
+    public boolean isValidRG(String rg, int index) {
 
+
+        if (index == rg.length()-1) {
+            int dv = (sumRg %11 == 0) ? 0 : 11-(sumRg %11);
+
+            if(dv == 10 && String.valueOf(rg.charAt(8)).toLowerCase().equals("x")) {
+                return true;
+            }else if (dv == Integer.parseInt(String.valueOf(rg.charAt(8)))) {
+                return true;
+            }
+
+            return false;
+        }
+
+        char currentChar = rg.charAt(index);
+
+        switch (currentChar) {
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                if (index !=8) {
+                    sumRg += Integer.parseInt(String.valueOf(currentChar)) * poundRg;
+                    poundRg++;
+                }
+                return isValidRG(rg, index + 1);
+
+            default:
+                return false;
+        }
     }
 
-    private void checkCharacters(final char symbol) {
-
+    private void isValidCpf() {
     }
 }
